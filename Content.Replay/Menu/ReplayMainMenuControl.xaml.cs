24b66d7bff5be227f5fbab1aa318bf0bfc814f10
GreaseMonk <1354802+GreaseMonk@users.noreply.github.com>
2025-07-26 18:32:18 +0200

    Branding (#2)
    * Add logo
    
    * Update window title
    
    * Add icons
    
    * Update logo
    
    * Update manifest.yml
    
    * Update logo.png.yml
    
    ---------
    
    Co-authored-by: Dvir <39403717+dvir001@users.noreply.github.com>

--- Content.Replay/Menu/ReplayMainMenuControl.xaml.cs
+++ Content.Replay/Menu/ReplayMainMenuControl.xaml.cs
@@ -23,7 +23,7 @@ public sealed partial class ReplayMainMenuControl : Control
         LayoutContainer.SetGrowHorizontal(VBox, LayoutContainer.GrowDirection.Begin);
 
         Subtext.FontOverride = resCache.GetFont("/Fonts/NotoSansDisplay/NotoSansDisplay-Bold.ttf", 24);
-        var logoTexture = resCache.GetResource<TextureResource>("/Textures/Logo/logo.png");
+        var logoTexture = resCache.GetResource<TextureResource>("/Textures/_PS/Logo/logo.png"); // Prospect
         Logo.Texture = logoTexture;
 
         LayoutContainer.SetAnchorPreset(InfoContainer, LayoutContainer.LayoutPreset.BottomLeft);
