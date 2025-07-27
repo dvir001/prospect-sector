813a75e1e7ea1101c526b52e18b72ca214e16a60
dvir001 <dvirf01@gmail.com>
2025-07-28 01:05:39 +0300

    Update actions_changelog_rss.py

--- Tools/actions_changelog_rss.py
+++ Tools/actions_changelog_rss.py
@@ -29,22 +29,22 @@ CHANGELOG_RSS_KEY = os.environ.get("CHANGELOG_RSS_KEY")
 
 # Change these to suit your server settings
 # https://docs.fabfile.org/en/stable/getting-started.html#run-commands-via-connections-and-run
-SSH_HOST = "moon.spacestation14.com"
+SSH_HOST = "changelog.prospect-sector.space"
 SSH_USER = "changelog-rss"
 SSH_PORT = 22
 RSS_FILE = "changelog.xml"
 XSL_FILE = "stylesheet.xsl"
 HOST_KEYS = [
-    "AAAAC3NzaC1lZDI1NTE5AAAAIOBpGO/Qc6X0YWuw7z+/WS/65+aewWI29oAyx+jJpCmh"
+    "AAAAC3NzaC1lZDI1NTE5AAAAIEpVVSmaRVZIMPY008YUjNPjmFD/v804Kc5t+Y/jv6cg"
 ]
 
 # RSS feed parameters, change these
-FEED_TITLE       = "Space Station 14 Changelog"
-FEED_LINK        = "https://github.com/space-wizards/space-station-14/"
-FEED_DESCRIPTION = "Changelog for the official Wizard's Den branch of Space Station 14."
+FEED_TITLE       = "Prospect Sector Changelog"
+FEED_LINK        = "https://github.com/Prospect-Sector/prospect-sector/"
+FEED_DESCRIPTION = "Changelog for the Prospect Sector branch of Space Station 14."
 FEED_LANGUAGE    = "en-US"
-FEED_GUID_PREFIX = "ss14-changelog-wizards-"
-FEED_URL         = "https://central.spacestation14.io/changelog.xml"
+FEED_GUID_PREFIX = "ss14-changelog-prospect-sector-"
+FEED_URL         = "https://changelog.prospect-sector.space/changelog.xml"
 
 CHANGELOG_FILE = "Resources/Changelog/Changelog.yml"
 
