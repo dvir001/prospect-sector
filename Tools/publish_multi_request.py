f2b41152cb37aa596b53840458020c46f360eaa2
Dvir <39403717+dvir001@users.noreply.github.com>
2025-08-01 02:22:58 +0300

    Update CDN URLs to new ss14-cdn domain (#13)

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.prospect-sector.space/"
+ROBUST_CDN_URL = "https://ss14-cdn.prospect-sector.space/"
 FORK_ID = "prospect-sector"
 
 def main():
