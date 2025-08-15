626510a9f42303e9b591f524457cf963e3680f97
GreaseMonk <1354802+GreaseMonk@users.noreply.github.com>
2025-08-15 19:20:08 +0200

    Cleanup

--- Content.Server/_PS/Prospectable/ProspectableSystem.cs
+++ Content.Server/_PS/Prospectable/ProspectableSystem.cs
@@ -6,14 +6,11 @@ namespace Content.Server._PS.Prospectable;
 public sealed class ProspectableSystem: SharedProspectableSystem
 {
     [Dependency] private readonly SharedMapSystem _map = default!;
-    [Dependency] private readonly IEntityManager _ent = default!;
     [Dependency] private readonly ILogManager _log = default!;
-    private ISawmill _logger = default!;
 
     public override void Initialize()
     {
         base.Initialize();
-        _logger = _log.GetSawmill("prospectable");
         SubscribeLocalEvent<RandomItemSpawnedEvent>(OnRandomItemSpawnedEvent);
     }
 
@@ -35,7 +32,7 @@ public sealed class ProspectableSystem: SharedProspectableSystem
     private void GetMapLevel(EntityUid entityUid, out int mapLevel)
     {
         var mapUid = _map.GetMap(Transform(entityUid).MapID);
-        if (_ent.TryGetComponent<TerradropMapComponent>(mapUid, out var comp))
+        if (TryComp<TerradropMapComponent>(mapUid, out var comp))
             mapLevel = comp.ThreatLevel;
         else
             mapLevel = MinItemLevel;
@@ -51,5 +48,4 @@ public sealed class ProspectableSystem: SharedProspectableSystem
         var comp = EnsureComp<ProspectableComponent>(entityUid);
         comp.Level = mapLevel;
     }
-
 }
